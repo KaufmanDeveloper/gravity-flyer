9c7f0423e5ff5832dbb7c2c4a804ea01dd5390e5
Michael Kaufman <mkaufman94@gmail.com>
2019-02-10 17:35:34 -0500

    feat: Implement player movement (#1)
    * feat: Allow player to move left and right
    
    * feat: Left and right invidually working
    
    * fix: Properly slide in either direction

---
+++ Gravity Flyer/player/Player.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+
+const initialSpeed = 120
+var moveDirection = Vector2(0, 0)
+var velocity = Vector2(0, 0)
+var gravity = .90
+var previous = "none"
+
+
+func _physics_process(delta):
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+
+	controls_loop(left, right)
+	movement_loop(left, right)
+
+func controls_loop(left, right):
+	moveDirection.x = -int(left) + int(right)
+
+func movement_loop(left, right):
+	if left:
+		if previous == "left":
+			if velocity.x < 0:
+				velocity.x = velocity.x / gravity
+			else:
+				velocity.x -= initialSpeed
+		
+		if !velocity.x < -initialSpeed: # Subtracting from other side
+			velocity.x -= initialSpeed
+		
+		previous = "left"
+	
+	if right:
+		if previous == "right":
+			if velocity.x > 0:
+				velocity.x = velocity.x / gravity
+			else:
+				velocity.x += initialSpeed
+		if !velocity.x > initialSpeed: # Adding from other side
+			velocity.x += initialSpeed
+		
+		previous = "right"
+	
+	if !left and !right:
+		velocity.x *= gravity
+		previous = "none"
+	
+	print(velocity.x)
+	
+	move_and_slide(velocity, Vector2(0, 0))
