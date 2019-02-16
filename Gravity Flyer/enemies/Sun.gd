48061b024e27a112fca85f4bfad74bb411dd9b5e
Michael Kaufman <mkaufman94@gmail.com>
2019-02-16 15:05:06 -0500

    feat: Create sun spawner and add sword swing on both sides (#3)
    * feat: Create spawner object
    
    * feat: Set spawn timer
    
    * feat: Ability to swing on both sides
    
    * fix: Correct collision mask for sword
    
    * feat: Do not repeat animation for same direction
    
    * fix: Seperate blink animation

--- Gravity Flyer/enemies/Sun.gd
+++ Gravity Flyer/enemies/Sun.gd
@@ -4,5 +4,14 @@ extends RigidBody2D
 # var a = 2
 # var b = "textvar"
 
+var offset = Vector2(0, 0)
+var impulse = Vector2(200, 200)
+var startingPosition = Vector2(100, 0)
+
 func _ready():
 	$Animation.play("idle")
+	apply_movement()
+	position = startingPosition
+
+func apply_movement():
+	apply_impulse(offset, impulse)
--- Gravity Flyer/player/Player.gd
+++ Gravity Flyer/player/Player.gd
@@ -5,7 +5,12 @@ var moveDirection = Vector2(0, 0)
 var velocity = Vector2(0, 0)
 var gravity = .90
 var previous = "none"
+var previousAnimation = "right"
+var playingAnimation = "idleright"
 
+func _ready():
+	$Animation.play(playingAnimation)
+	$BlinkAnimation.play("blink")
 
 func _physics_process(delta):
 	var left = Input.is_action_pressed("ui_left")
@@ -13,9 +18,30 @@ func _physics_process(delta):
 
 	controls_loop(left, right)
 	movement_loop(left, right)
+	
+	if Input.is_action_just_pressed("swipe"):
+		# use_item(preload("res://items/Huff.tscn"))
+		use_item(preload("res://player/Sword.tscn"), "right")
+		use_item(preload("res://player/Sword.tscn"), "left")
+
+func use_item(item, side):
+	var newItem = item.instance() # Argument is a direct path to that item scene
+	newItem.add_to_group(str(newItem.get_name(), self))
+	# newItem.add_to_group(str(item, self))
+	newItem.side = side
+	add_child(newItem)
+	if get_tree().get_nodes_in_group(str(newItem.get_name(), self)).size() > newItem.maxAmount:
+	# if get_tree().get_nodes_in_group(str(item, self)).size() > newItem.maxamount:
+		newItem.queue_free()
 
 func controls_loop(left, right):
 	moveDirection.x = -int(left) + int(right)
+	if left and !right and previousAnimation == "right":
+		previousAnimation = "left"
+		$Animation.play("idleleft")
+	elif right and !left and previousAnimation == "left":
+		previousAnimation = "right"
+		$Animation.play("idleright")
 
 func movement_loop(left, right):
 	if left:
