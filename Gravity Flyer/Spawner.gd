e12410d00bc52f52d1aa8cdb541bbef1c97d5058
Michael Kaufman <mkaufman94@gmail.com>
2019-02-16 13:20:15 -0500

    feat: Set spawn timer

--- Gravity Flyer/Spawner.gd
+++ Gravity Flyer/Spawner.gd
@@ -3,6 +3,16 @@ extends Node
 var timer = 0
 
 func _ready():
+	pass
+
+func _process(delta):
+	if (timer == 0):
+		spawn_sun()
+		timer = 360
+	timer -= 1
+
+
+func spawn_sun():
 	var sun = preload("res://enemies/Sun.tscn")
 	var sunInstance = sun.instance()
 	sunInstance.add_to_group(str(sunInstance.get_name(), self))
