32bc2e024bebb25e16131dfb05c36d69f38774f4
Michael Kaufman <mkaufman94@gmail.com>
2019-02-16 15:25:45 -0500

    feat: Working set impulse for overlapping area

--- Gravity Flyer/player/Sword.gd
+++ Gravity Flyer/player/Sword.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+const TYPE = "SWORD"
+
 var maxAmount = 1 # How many of this item can be active at once
 var side = "right"
+var impulse = Vector2(200, 200)
+var startingPosition = Vector2(100, 0)
 
 func _ready():
 	$Animation.connect("animation_finished", self, "destroy")
 	$Animation.play(str("swing") + side)
+
+func _process(delta):
+	for area in $Area.get_overlapping_areas():
+		var body = area.get_parent()
+		if body.get("TYPE"):
+			if body.get("TYPE") == "ENEMY":
+				body.apply_impulse(Vector2(0, 0), impulse)
 
 func destroy(animation):
 #	if get_parent().has_method("state_swing"):
